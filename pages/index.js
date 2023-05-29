885f5ddbce7862dd9c7a7d6772d83a23254cd79e
EmFor2001 <k.hajduk.kh@gmail.com>
2023-05-29 13:24:48 +0200

    additional content

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import {
 } from "@react-three/drei";
 import Head from "next/head";
 import React, { useEffect } from "react";
+import styled from "styled-components";
 import {
   EffectComposer,
   GammaCorrectionShader,
@@ -118,8 +119,8 @@ const Landscape = () => {
 
   useFrame((state) => {
     // Update plane position
-    terrain1Ref.current.position.z = (state.clock.elapsedTime * 0.15) % 2;
-    terrain2Ref.current.position.z = ((state.clock.elapsedTime * 0.15) % 2) - 2;
+    terrain1Ref.current.position.z = (state.clock.elapsedTime * 0.1) % 2;
+    terrain2Ref.current.position.z = ((state.clock.elapsedTime * 0.1) % 2) - 2;
   });
 
   return (
@@ -188,7 +189,7 @@ const Light = () => {
     <>
       <spotLight
         ref={spotlight1Ref}
-        color="#d53c3d"
+        color="#ff068c"
         intensity={10}
         position={[0.5, 0.75, 2.1]}
         distance={25}
@@ -198,7 +199,7 @@ const Light = () => {
       />
       <spotLight
         ref={spotlight2Ref}
-        color="#d53c3d"
+        color="#ff068c"
         intensity={10}
         position={[-0.5, 0.75, 2.1]}
         distance={25}
@@ -209,21 +210,6 @@ const Light = () => {
     </>
   );
 };
-
-
-const Foo = (props) => {
-  const { camera } = useThree();
-  const { scroll } = props;
-  
-  useEffect(() => {
-  camera.position.set(0, 0.1, (-2.4+(scroll * 0.003)));
-  }, [scroll]);
-
-  return ( 
-    <>
-    </>
-  );
-}
 
 /**
  * This component renders the scene which renders all the components declared above and more:
@@ -244,7 +230,6 @@ const Foo = (props) => {
  */
 const Scene = (props) => {
   const [mounted, setMounted] = React.useState(false);
-  const { scroll } = props;
 
   React.useEffect(() => {
     setMounted(true);
@@ -280,9 +265,8 @@ const Scene = (props) => {
             <Light />
             <Landscape />
             <Effects />
-            <Foo scroll={scroll}/>
             {/* <Cube /> */}
-            <LogoObject />
+            {/* <LogoObject /> */}
           </React.Suspense>
         </Canvas>
       )}
@@ -291,8 +275,6 @@ const Scene = (props) => {
 };
 
 export default function Home() {
-  const [scroll, setScroll] = React.useState(0);
-
   return (
     <div>
       <Head>
@@ -304,18 +286,53 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="label-container"
-          onScroll={(e) => {
-            setScroll(e.currentTarget.scrollTop);
-          }}
-        >
-            <p className="text">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, distinctio. Cum ex tenetur expedita atque porro voluptas iure consequuntur odio repellat aperiam similique earum iste possimus molestiae neque, placeat maxime.
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita harum temporibus voluptate eos optio minima cupiditate quaerat nulla! Dolores consectetur repudiandae aut exercitationem illum repellat blanditiis, laudantium optio laboriosam molestias.
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet magnam labore quod qui laudantium est sunt, alias saepe. Qui quos suscipit aperiam cumque nemo ipsam laborum vel rerum est id.
-            </p>
+        <div className="animation__content__container">
+          <img className="logo__jetski" src="/logo_jetskipsd.png" alt="" />
+          <a href="./#intro__section" className="arrow"><img src="/circle-down-solid.svg" alt="" /></a>
         </div>
-        <Scene scroll={scroll}/>
+        <div className="content__container">
+          <section className="intro__section" id="intro__section">
+            <div className="intro__img__container">
+              <img className="intro__img" src="/jetski.webp" alt="" />
+            </div>
+            <div className="intro__text__container">
+              <h1 className="intro__title">O nas</h1>
+              <p className="intro__text">
+                Oferujemy wynajem skuterów wodnych, na terenie całego trójmiasta wraz z dowozem sprzętu.
+                Nasze skutery wodne podlegają regularnym przeglądom technicznym, dzięki czemu możemy zagwarantować bezpieczeństwo i komfort podczas jazdy.
+              </p>
+            </div>
+          </section>
+
+          <section className="table__container">
+            <div className="table__content">
+              <h1 className="table__title">Cennik</h1>
+              <table className="table">
+                <tr>
+                  <th>Czas</th>
+                  <th>Cena</th>
+                </tr>
+                <tr>
+                  <td>1h</td>
+                  <td>300zł</td>
+                </tr>
+                <tr>
+                  <td>24h</td>
+                  <td>600zł</td>
+                </tr>
+                <tr>
+                  <td>3 - 9 dni</td>
+                  <td>500zł / doba</td>
+                </tr>
+                <tr>
+                  <td>10+ dni</td>
+                  <td>460zł / doba</td>
+                </tr>
+              </table>
+            </div>
+          </section>
+        </div>
+        <Scene/>
       </main>
     </div>
   );
